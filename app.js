818cfbd63c034452d2eb4491ee23d3d18a890e12
Michal <viniarski@gmail.com>
2024-01-18 14:48:59 +0000

    add event nom nom nom and bug fix

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setInterval(updateCounter, 1000);
+  setInterval(updateCounter, 1000);
 
-    // Counter increment on clicking cookieButton
-    const cookieButton = document.getElementById("cookieButton");
-    cookieButton.addEventListener("click", incrementCounter);
+  const cookieButton = document.getElementById("cookieButton");
+  cookieButton.addEventListener("click", incrementCounter);
+  const resetButton = document.getElementById("monsterButton");
+  resetButton.addEventListener("click", resetCounter);
 });
 
 let counter = 0;
 
 function updateCounter() {
-    counter++;
-    const cookieCountElement = document.getElementById("cookieCount");
-    cookieCountElement.textContent = `${counter} Cookies`;
+  counter++;
+  const cookieCount = document.getElementById("cookieCount");
+  cookieCount.textContent = `${counter} Cookies`;
 }
 
 function incrementCounter() {
-    counter++;
-    updateCounter();
+  counter++;
+  updateCounter();
 }
+
+function resetCounter() {
+    counter = 0;
+    const cookieCount = document.getElementById("cookieCount");
+    cookieCount.textContent = `${counter} Cookies`;
+  
+    const eventMessage = document.getElementById("eventMessage");
+    eventMessage.textContent = "Nom nom nom";
+    eventMessage.style.visibility = 'visible';
+  
+    setTimeout(() => {
+      eventMessage.textContent = "";
+      eventMessage.style.visibility = 'hidden';
+    }, 500);
+  }
+  
